0be24a6f180bf218dd71d28e6357198857d3633e
asdwsxzc123 <sbfgeyygy@126.com>
2019-05-11 20:38:47 +0800

    优化

---
+++ study/deploy/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+GIT_HOME=/root/git/
+DEST_PATH=/root/project/front/
+
+if [ ! -n "$1"];
+then
+  echo -e "please input a project name! you can input as follows:"
+  echo -e "./deploy.sh react-dom"
+  exit
+fi
+
+if [ $1 = "react-dom"];
+then
+  echo -e "--------enter project--------"
+  cd $GIT_HOME$1;
+else
+  echo -e "Invalid project name!"
+  exit
+fi
+
+# clean dist
+echo -e "-------- clean dist --------"
+rm -rf ./dist
+
+echo -e "------git pull-----"
+git pull
+
+echo -e "-----yarn install-----"
+yarn
+
+echo -e "-----yarn run dist-----"
+yarn dist
+
+if [-d "./dist"];
+then 
+  echo -e "------- clean dist ---------"
+  rm -rf $DEST_PATH/dist
+
+  echo -e "------- copy dist ---------"
+  cp -R $DEST_PATH/$1/
+
+  echo -e "------- deploy success ---------"
+else 
+  echo -e " ------- deploy faile ---------"
+fi
